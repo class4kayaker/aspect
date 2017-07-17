412246fa4bb2f32204649d08fff08ed41d14b6f7
Jonathan Robey <class4kayaker@gmail.com>
2017-07-17 13:27:12 -0700

    Add additional assembler work

--- include/aspect/vof/assembly.h
+++ include/aspect/vof/assembly.h
@@ -159,9 +159,24 @@ namespace aspect
     class VoFAssembler : public SimulatorAccess<dim>
     {
       public:
-          /**
-           * Function for assembling face fluxes for VoF system.
-           */
+        void local_assemble_vof_system (const VoFField<dim> field,
+                                        const unsigned int calc_dir,
+                                        bool update_from_old,
+                                        const typename DoFHandler<dim>::active_cell_iterator &cell,
+                                        internal::Assembly::Scratch::VoFSystem<dim> &scratch,
+                                        internal::Assembly::CopyData::VoFSystem<dim> &data);
+
+        void local_assemble_boundary_face_vof_system (const VoFField<dim> field,
+                                                      const unsigned int calc_dir,
+                                                      bool update_from_old,
+                                                      const typename DoFHandler<dim>::active_cell_iterator &cell,
+                                                      const unsigned int face_no,
+                                                      internal::Assembly::Scratch::VoFSystem<dim> &scratch,
+                                                      internal::Assembly::CopyData::VoFSystem<dim> &data);
+
+        /**
+         * Function for assembling face fluxes for VoF system.
+         */
         void local_assemble_internal_face_vof_system (const VoFField<dim> field,
                                                       const unsigned int calc_dir,
                                                       bool update_from_old,
--- source/vof/assembler.cc
+++ source/vof/assembler.cc
@@ -39,6 +39,27 @@ namespace aspect
   namespace Assemblers
   {
     template <int dim>
+    void VoFAssembler<dim>::local_assemble_vof_system (const VoFField<dim> field,
+                                                       const unsigned int calc_dir,
+                                                       bool update_from_old,
+                                                       const typename DoFHandler<dim>::active_cell_iterator &cell,
+                                                       internal::Assembly::Scratch::VoFSystem<dim> &scratch,
+                                                       internal::Assembly::CopyData::VoFSystem<dim> &data)
+    {
+    }
+
+    template <int dim>
+    void VoFAssembler<dim>::local_assemble_boundary_face_vof_system (const VoFField<dim> field,
+                                                                     const unsigned int calc_dir,
+                                                                     bool update_from_old,
+                                                                     const typename DoFHandler<dim>::active_cell_iterator &cell,
+                                                                     const unsigned int face_no,
+                                                                     internal::Assembly::Scratch::VoFSystem<dim> &scratch,
+                                                                     internal::Assembly::CopyData::VoFSystem<dim> &data)
+    {
+    }
+
+    template <int dim>
     void VoFAssembler<dim>::local_assemble_internal_face_vof_system (const VoFField<dim> field,
                                                                      const unsigned int calc_dir,
                                                                      bool update_from_old,
